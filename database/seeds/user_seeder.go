10845c05d611381ebfdbef49eb7b60072b4512f6
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-09-21 11:40:39 +0700

    feat(seeders) : add user seeder.

---
+++ database/seeds/user_seeder.go
@@ -0,0 +1,27 @@
+package seeds
+
+import (
+	"github.com/nullsec45/golang-news-api/internal/core/domain/model"
+	"github.com/nullsec45/golang-news-api/lib/conv"
+	"github.com/rs/zerolog/log"
+	"gorm.io/gorm"
+)
+
+func SeedRoles(db *gorm.DB) {
+	bytes, err := conv.HashPassword("admin123")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error creating password hash")
+	}
+
+	admin := model.User{
+		Name:"Admin",
+		Email:"admin@gmail.com",
+		Password:string(bytes),
+	}
+
+	if err := db.FirstOrCreate(&admin, model.User{Email:"admin@gmail.com"}).Error; err != nil {
+		log.Fatal().Err(err).Msg("Error seeding admin role")
+	}else{
+		log.Info().Msg("Admin role seeded succesfully")
+	}
+}
