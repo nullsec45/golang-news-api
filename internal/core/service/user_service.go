3e7d92891a1f8f6c185d57bd0e27f4fe6b077534
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-09-25 17:18:47 +0700

    feat (user) : user service.

---
+++ internal/core/service/user_service.go
@@ -0,0 +1,53 @@
+package service
+
+import (
+	"github.com/nullsec45/golang-news-api/internal/adapter/repository"
+	"github.com/nullsec45/golang-news-api/internal/core/domain/entity"
+	"github.com/nullsec45/golang-news-api/lib/conv"
+	"context"
+
+	"github.com/gofiber/fiber/v2/log"
+)
+
+type UserService interface {
+	UpdatePassword(ctx context.Context, newPass string, id int64) error
+	GetUserByID(ctx context.Context, id int64) (*entity.UserEntity, error)
+}
+
+type userService struct {
+	userRepo repository.UserRepository
+}
+
+// GetUserByID implements UserService.
+func (u *userService) GetUserByID(ctx context.Context, id int64) (*entity.UserEntity, error) {
+	result, err := u.userRepo.GetUserByID(ctx, id)
+	if err != nil {
+		code := "[SERVICE] GetUserByID - 1"
+		log.Errorw(code, err)
+		return nil, err
+	}
+	return result, nil
+}
+
+// UpdatePassword implements UserService.
+func (u *userService) UpdatePassword(ctx context.Context, newPass string, id int64) error {
+	password, err := conv.HashPassword(newPass)
+	if err != nil {
+		code := "[SERVICE] UpdatePassword - 1"
+		log.Errorw(code, err)
+		return err
+	}
+
+	err = u.userRepo.UpdatePassword(ctx, password, id)
+	if err != nil {
+		code := "[SERVICE] UpdatePassword - 2"
+		log.Errorw(code, err)
+		return err
+	}
+
+	return nil
+}
+
+func NewUserService(userRepo repository.UserRepository) UserService {
+	return &userService{userRepo: userRepo}
+}
